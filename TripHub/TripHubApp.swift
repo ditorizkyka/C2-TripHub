4c658841df16dcbcb91b3dcb3e5ca6c82d5363b1
ditorizkyka <rizkykandito@gmail.com>
2026-04-30 17:14:25 +0700

    Initial Commit

---
+++ TripHub/TripHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripHubApp.swift
+//  TripHub
+//
+//  Created by Andito Rizkyka Rianto on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct TripHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
